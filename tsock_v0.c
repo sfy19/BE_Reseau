5fc1a741a4608f312b8f5db8ebf478d0aff9b260
Housbani Soufiane <housbani@insa-20042>
2022-02-09 15:19:59 +0100

    V2

--- tsock_v0.c
+++ tsock_v0.c
@@ -17,13 +17,6 @@ données du réseau */
 #include <stdio.h>
 /* pour la gestion des erreurs */
 #include <errno.h>
-
-void main (int argc, char **argv)
-{
-	/* ---------------------------declaration des paramètre--------------------------------------- */ 
-	int c;
-	extern char *optarg;
-      
  //sfy_omom: #include <sys/types.h>
 /* constantes relatives aux domaines, types et protocoles */
 #include <sys/socket.h>
@@ -39,25 +32,125 @@ données du réseau */
 /* pour la gestion des erreurs */
 #include <errno.h>
 
+void construire_message (char *msg, char motif, int lg_message) {
+    int i;
+    for (i=0 ; i<lg_message; i++) msg[i] = motif; 
+}
+
+void afficher_message (char *message, int lg) {
+    int i;
+    printf("message construit : ");
+    for (i=0 ; i<lg ; i++) printf("%c", message[i]) ; printf("\n"); 
+}
+
+
+void puits_udp(int port, int lg_message, int nbre_messages) {
+    // Création socket
+    int sock; 
+    sock=socket(AF_INET,SOCK_DGRAM,0);
+    if ( sock == -1) {
+        printf("PUITS : Erreur de création de socket \n"); 
+        exit(-1); 
+    }
+    // D&finition @ locale
+    //declaration
+    struct sockaddr_in adr_local;
+    int lg_adr = sizeof(adr_local); 
+    memset ((char*)&adr_local,0,sizeof(adr_local));
+    adr_local.sin_family = AF_INET;
+    adr_local.sin_port=htons(port);
+    adr_local.sin_addr.s_addr=INADDR_ANY; 
+    // association @locale & socket local
+    int res_bind = bind (sock,(struct sockaddr *) &adr_local,lg_adr); 
+    if (res_bind == -1) {
+        printf("Erreur bind \n");
+        exit(1); 
+    }
+    // réception msgs 
+
+    char * msg = malloc(sizeof(lg_message));
+    struct sockaddr_in adr_em; 
+    int lg_adr_em= sizeof(adr_em);
+    int octets_recus; 
+
+    if (nbre_messages == -1) { // Reception infinie
+        while (1) {
+            octets_recus= recvfrom(sock,msg,lg_message,0,(struct sockaddr *) &adr_em, &lg_adr_em);
+            if (octets_recus == -1) {
+                printf("Erreur recevfrom \n");
+                exit(1); 
+            }
+            afficher_message(msg, lg_message);  
+        }
+    } else {
+        for (int i=0; i < nbre_messages; i++) {
+            octets_recus= recvfrom(sock,msg,lg_message,0,(struct sockaddr *) &adr_em, &lg_adr_em);
+            if (octets_recus == -1) {
+                printf("Erreur recevfrom \n");
+                exit(1); 
+            }
+            afficher_message(msg, lg_message);  
+        }
+    }
+
+    // destruction du socket
+    close(sock); 
+}
+
+void source_udp(int port, char* destination, int lg_message, int nbre_messages) {
+// Création socket
+  int sock; 
+    sock=socket(AF_INET,SOCK_DGRAM,0);
+    if ( sock == -1) {
+        printf("PUITS : Erreur de création de socket \n"); 
+        exit(-1); 
+    }
+    // D&finition @ locale
+    //declaration
+    struct sockaddr_in adr_local;
+    int lg_adr = sizeof(adr_local); 
+    memset ((char*)&adr_local,0,sizeof(adr_local));
+    adr_local.sin_family = AF_INET;
+    adr_local.sin_port=htons(port);
+// Définition @ distante
+    struct hostent *hp;
+    struct sockaddr_in adr_distant;
+    int lg_adrd = sizeof(adr_distant);
+    memset((char*)&adr_distant,0,sizeof(adr_distant));
+    adr_distant.sin_port=9000;
+    if((hp = gethostbyname("dumas.insa-tlse.fr")) == NULL){
+    printf("erreur gethostbyname\n");
+    exit(1);
+    
+    }
+    memcpy((char*)&(adr_distant.sin_addr.s_addr),hp->h_addr,hp->h_length);
+    
+//envoi msg vers @distancte
+    if {
+    sendto(sock,*msg,lg_message,0,*adr_distant,lg_adr);
+    print("%d",lg_message);}
+    else{
+    print("-1");}
+// affichage msg 
+// destruction du socket
+    close(sock);
+}
+
 void main (int argc, char **argv)
 {
-	/* ---------------------------declaration des paramètre--------------------------------------- */ 
 	int c;
 	extern char *optarg;
 	extern int optind;
 	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
 	int source = -1 ; /* 0=puits, 1=source */
-	char *dest ;
-	int lg =30; /* longueur du message par défaut */
-	int tcp=1;
-	int port;
-	
-	/*------------------------------Début--------------------------------*/ 
-	while ((c = getopt(argc, argv, "pn:su")) != -1) {
+	int tcp =1; 
+	int lg =30; 
+	int port = atoi(argv[argc-1]);
+	while ((c = getopt(argc, argv, "upn:s")) != -1) {
 		switch (c) {
 		case 'p':
 			if (source == 1) {
-				printf("Warning :\n On ne peut être source et puits en même temps, Please enlever une des deux options -s ou -p\n\n");
+				printf("usage: cmd [-p|-s][-n ##]\n");
 				exit(1);
 			}
 			source = 0;
@@ -65,73 +158,56 @@ void main (int argc, char **argv)
 
 		case 's':
 			if (source == 0) {
-				printf("Warning :\n On ne peut être source et puits en même temps, Please enlever une des deux options -s ou -p\n\n");
+				printf("usage: cmd [-p|-s][-n ##]\n");
 				exit(1) ;
 			}
 			source = 1;
 			break;
-		case 'u':
-			tcp=0;
-			printf("l'envoi se fait via udp");
-			break;
-				       
+
 		case 'n':
 			nb_message = atoi(optarg);
 			break;
+			
+	    case 'u': 
+	        tcp = 0; 
+	        break; 
 
 		default:
-			printf("usage: ./tsock [-p|-s][-n ##]\n");
+			printf("usage: cmd [-p|-s][-n ##]\n");
 			break;
 		}
 	}
-	if ((port=atoi(argv[argc-1]))!=-1)   //récupération du numéro de port 
-    {
-        port=htons(port);         //   pour la précaution en utilise la fonction htons      
-    }
-
-  	
-				       
 
 	if (source == -1) {
-		printf("usage: ./tsock [-p|-s][-n ##]\n");
+		printf("usage: cmd [-p|-s][-n ##]\n");
 		exit(1) ;
 	}
-	
 
-	if (source == 1){
+    char * dest = argv[argc-2]; 
+	if (source == 1)
 		printf("on est dans le source\n");
-	}
+		
 	else
-	  {
-	    printf("on est dans le puits\n");
-	  }
-	
-	if (tcp==1)
-	  {
-            printf("Protocole de transport : TCP | ");
-	  }
-        else
-	  {
-            printf("Protocole de transport : UDP | ");
-	  }			       
+		printf("on est dans le puits\n");
+
 	if (nb_message != -1) {
-	  if (source == 1){
+		if (source == 1)
 			printf("nb de tampons à envoyer : %d\n", nb_message);
-	  }
-	  else
+		else
 			printf("nb de tampons à recevoir : %d\n", nb_message);
 	} else {
 		if (source == 1) {
 			nb_message = 10 ;
 			printf("nb de tampons à envoyer = 10 par défaut\n");
 		} else
-		printf("nb de tampons à envoyer = infini\n");
+		printf("nb de tampons à recevoir = infini\n");
 
 	}
-	dest=argv[argc-2];
-	printf("le nom de la machine est: %s \n",dest);
-}
- 
-
+	
+	if (source ==0) { //puits 
+	    if (tcp == 0) {
+	        puits_udp(port, lg, nb_message);  
+	       }
+	} 
 }
 
